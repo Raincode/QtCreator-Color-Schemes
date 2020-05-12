71dad53111a0d0b4568940c796856eb77f164867
Matthias Stauber <matthias.stauber@gmx.de>
2020-05-12 22:42:51 +0200

    Fixed install windows

--- install_windows.bat
+++ install_windows.bat
@@ -1,7 +1,10 @@
 @echo off
 
+
 xcopy /l /f .\styles %USERPROFILE%\AppData\Roaming\QtProject\qtcreator\styles
 
+
 xcopy /l /f .\themes %USERPROFILE%\AppData\Roaming\QtProject\qtcreator\themes
+
 
 Pause
