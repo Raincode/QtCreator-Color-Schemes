6e10737f1e5e59ee359f9ac23479a14d375b9a34
Matthias Stauber <matthias.stauber@gmx.de>
2023-04-23 17:00:42 +0200

    Minor updates on install scripts
    Signed-off-by: Matthias Stauber <matthias.stauber@gmx.de>

--- install_linux.bash
+++ install_linux.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Experimental... not yet tested
-
 QT_DIR=~/.config/QtProject/qtcreator
 
 # Copy styles
--- install_windows.bat
+++ install_windows.bat
@@ -1,5 +1,11 @@
 @echo off
 
-xcopy .\styles %USERPROFILE%\AppData\Roaming\QtProject\qtcreator\styles\
-xcopy .\themes %USERPROFILE%\AppData\Roaming\QtProject\qtcreator\themes\
+set "QT_DIR=%USERPROFILE%\AppData\Roaming\QtProject\qtcreator"
+
+REM Copy styles
+xcopy .\styles %QT_DIR%\styles\
+
+REM Copy themes
+xcopy .\themes %QT_DIR%\themes\
+
 Pause
